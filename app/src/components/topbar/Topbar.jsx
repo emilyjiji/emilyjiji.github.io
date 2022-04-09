19e85c6596328f62f192bb859adda462436e3b5f
emilyjiji <emilyjiji@ufl.edu>
2022-04-09 00:46:56 -0400

    hello`

---
+++ app/src/components/topbar/Topbar.jsx
@@ -0,0 +1,30 @@
+import "./topbar.scss";
+import {Person, Mail} from "@mui/icons-material"
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+  return (
+    <div className = {"topbar " + (menuOpen && "active")}>
+      <div className = "wrapper">
+        <div className = "left">
+          <a href="#intro" className = "logo">genius.</a>
+          {/* <div className="logo">genius.</div> */}
+          <div className="itemContainer">
+           <Person className = "icon"/>
+           <span>561-639-9339</span>
+          </div>
+          <div className = "itemContainer">
+            <Mail className = "icon"/>
+            <span>emilyjiji@ufl.edu</span>
+          </div>
+        </div>
+        <div className = "right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className = "line1"></span>
+            <span className = "line2"></span>
+            <span className = "line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
